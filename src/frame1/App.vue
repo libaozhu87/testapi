7026dc1141390957f326f416cb1aaf0855c2d44d
洪玄 <abbott.zh@alibaba-inc.com>
2018-07-20 20:26:19 +0800

    commit message

---
+++ src/frame1/App.vue
@@ -0,0 +1,431 @@
+<template>
+  <div>
+    <div id="g44" class="white-column-flex-start-w750-g44">
+      <div id="g24" class="row-space-between-w750-g24">
+        <div id="t22" class="black-size34-left-t22">租赁更时111尚11</div>
+        <div id="t23" class="gray-size24-right-t23"> 张**嘉5分钟前租了相机</div>
+      </div>ˆ
+      <div id="g42" class="row-center-w750-g42">
+        <div id="g34" class="white-row-flex-start-g34">
+          <div id="a45" class="column-center-background-a45">
+            <div id="s11" class="white-s11"></div>
+          </div>
+          <div id="g33" class="column-flex-start-g33">
+            <div id="g27" class="row-flex-start-g27">
+              <img id="i15" class="w48-h48-i15" src='https://gw.alicdn.com/tfs/TB1iW21FTtYBeNjy1XdXXXXyVXa-48-48.png' />
+              <div id="t12" class="yellow-size30-t12">闲鱼租房</div>
+              <div id="g26" class="row-center-w96-h28-g26">
+                <img id="i13" class="w96-h28-background-i13" src='https://gw.alicdn.com/tfs/TB1pC4IFHSYBuNjSspiXXXNzpXa-96-28.png' />
+                <div id="t14" class="white-size22-t14">免中介费</div>
+              </div>
+            </div>
+            <div id="t16" class="gray-size24-t16">真实房东 超多房源</div>
+            <div id="g32" class="row-flex-start-g32">
+              <div id="g30" class="column-flex-start-w128-h128-left-g30">
+                <img id="i17" class="w128-h128-background-i17" src='https://gw.alicdn.com/tfs/TB1Zm4IFHSYBuNjSspiXXXNzpXa-128-128.png' />
+                <div id="g29" class="row-center-w74-h32-g29">
+                  <img id="i18" class="w74-h32-background-i18" src='https://gw.alicdn.com/tfs/TB1PrtqFKuSBuNjy1XcXXcYjFXa-74-32.png' />
+                  <div id="t19" class="white-size20-t19">0.5Km</div>
+                </div>
+              </div>
+              <div id="g31" class="column-flex-start-w128-h128-right-g31">
+                <img id="i20" class="w128-h128-background-i20" src='https://gw.alicdn.com/tfs/TB1W7JPFNSYBuNjSspjXXX73VXa-128-128.png' />
+                <img id="i21" class="w128-h128-i21" src='https://gw.alicdn.com/tfs/TB1cYxqFKuSBuNjy1XcXXcYjFXa-128-128.png' /> </div>
+            </div>
+          </div>
+        </div>
+        <div id="g37" class="white-column-center-g37">
+          <div id="t8" class="black-size30-t8">租衣服</div>
+          <div id="t9" class="gray-size24-t9">信用免押金</div>
+          <img id="i7" class="w126-h122-i7" src='https://gw.alicdn.com/tfs/TB1PWY1FTtYBeNjy1XdXXXXyVXa-126-122.png' /> </div>
+        <div id="g40" class="white-column-center-g40">
+          <div id="t4" class="black-size30-t4">租数码</div>
+          <div id="t5" class="gray-size24-t5">最低20元/天</div>
+          <img id="i3" class="w124-h126-i3" src='https://gw.alicdn.com/tfs/TB1R90IFHSYBuNjSspiXXXNzpXa-124-126.png' /> </div>
+      </div>
+      <img id="i1" class="w36-h12-i1" src='https://gw.alicdn.com/tfs/TB1kqY1FTtYBeNjy1XdXXXXyVXa-36-12.png' /> </div>
+  </div>
+</template>
+<style scoped>
+.white-column-flex-start-w750-g44 {
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  position: relative;
+  width: 750px;
+  background-color: rgba(255, 255, 255, 1);
+  display: flex;
+}
+
+.row-space-between-w750-g24 {
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+  position: relative;
+  width: 750px;
+  align-self: center;
+  margin-top: 24px;
+  display: flex;
+}
+
+.black-size34-left-t22 {
+  position: relative;
+  align-self: flex-end;
+  font-family: PingFangSC-Semibold;
+  font-size: 34px;
+  color: #222222;
+  line-height: 48px;
+  font-weight: normal;
+  margin-left: 32px;
+}
+
+.gray-size24-right-t23 {
+  position: relative;
+  align-self: flex-end;
+  font-family: PingFangSC-Regular;
+  font-size: 24px;
+  color: #888888;
+  line-height: 40px;
+  font-weight: normal;
+  margin-top: 8px;
+  margin-right: 32px;
+}
+
+.row-center-w750-g42 {
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  position: relative;
+  width: 750px;
+  align-self: center;
+  margin-top: 16px;
+  display: flex;
+}
+
+.white-row-flex-start-g34 {
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  position: relative;
+  align-self: flex-start;
+  background-color: rgba(245, 246, 247, 1);
+  border-radius: 12px;
+  display: flex;
+}
+
+.column-center-background-a45 {
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  display: flex;
+}
+
+.white-s11 {
+  position: relative;
+  background-color: rgba(245, 246, 247, 1);
+  border-radius: 12px;
+}
+
+.column-flex-start-g33 {
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  position: relative;
+  align-self: center;
+  margin-right: 35px;
+  margin-bottom: 0.5px;
+  margin-left: 17px;
+  display: flex;
+}
+
+.row-flex-start-g27 {
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  position: relative;
+  align-self: flex-start;
+  margin-right: 17px;
+  display: flex;
+}
+
+.w48-h48-i15 {
+  position: relative;
+  width: 48px;
+  height: 48px;
+  align-self: flex-start;
+  resize: stretch;
+}
+
+.yellow-size30-t12 {
+  position: relative;
+  align-self: flex-start;
+  font-family: PingFangSC-Medium;
+  font-size: 30px;
+  color: #ff8c21;
+  line-height: 32px;
+  font-weight: bold;
+  margin-top: 4px;
+  margin-bottom: 12px;
+  margin-left: 2px;
+}
+
+.row-center-w96-h28-g26 {
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 96px;
+  height: 28px;
+  align-self: flex-start;
+  margin-top: 6px;
+  margin-bottom: 14px;
+  margin-left: 8px;
+  display: flex;
+}
+
+.w96-h28-background-i13 {
+  position: absolute;
+  width: 96px;
+  height: 28px;
+  left: 0;
+  top: 0;
+  resize: stretch;
+}
+
+.white-size22-t14 {
+  position: relative;
+  align-self: center;
+  font-family: PingFangSC-Medium;
+  font-size: 22px;
+  color: #ffffff;
+  line-height: 20px;
+  font-weight: bold;
+}
+
+.gray-size24-t16 {
+  position: relative;
+  align-self: flex-start;
+  font-family: PingFangSC-Regular;
+  font-size: 24px;
+  color: #888888;
+  line-height: 28px;
+  font-weight: normal;
+  margin-top: -4px;
+  margin-right: 85px;
+  margin-left: 6px;
+}
+
+.row-flex-start-g32 {
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  position: relative;
+  align-self: flex-end;
+  margin-top: 11px;
+  margin-left: 19px;
+  display: flex;
+}
+
+.column-flex-start-w128-h128-left-g30 {
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  position: relative;
+  width: 128px;
+  height: 128px;
+  align-self: flex-start;
+  display: flex;
+}
+
+.w128-h128-background-i17 {
+  position: absolute;
+  width: 128px;
+  height: 128px;
+  left: 0;
+  top: 0;
+  resize: stretch;
+}
+
+.row-center-w74-h32-g29 {
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 74px;
+  height: 32px;
+  align-self: flex-start;
+  margin-top: 96px;
+  display: flex;
+}
+
+.w74-h32-background-i18 {
+  position: absolute;
+  width: 74px;
+  height: 32px;
+  left: 0;
+  top: 0;
+  resize: stretch;
+}
+
+.white-size20-t19 {
+  position: relative;
+  align-self: center;
+  font-family: PingFangSC-Medium;
+  font-size: 20px;
+  color: #ffffff;
+  line-height: 20px;
+  font-weight: bold;
+}
+
+.column-flex-start-w128-h128-right-g31 {
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  position: relative;
+  width: 128px;
+  height: 128px;
+  align-self: flex-start;
+  margin-left: 16px;
+  display: flex;
+}
+
+.w128-h128-background-i20 {
+  position: absolute;
+  width: 128px;
+  height: 128px;
+  left: 0;
+  top: 0;
+  resize: stretch;
+}
+
+.w128-h128-i21 {
+  position: relative;
+  width: 128px;
+  height: 128px;
+  align-self: flex-start;
+  resize: stretch;
+}
+
+.white-column-center-g37 {
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  align-self: flex-start;
+  background-color: rgba(245, 246, 247, 1);
+  border-radius: 12px;
+  margin-left: 16px;
+  padding-top: 22px;
+  padding-right: 18px;
+  padding-bottom: 20px;
+  padding-left: 18.5px;
+  display: flex;
+}
+
+.black-size30-t8 {
+  position: relative;
+  align-self: center;
+  font-family: PingFangSC-Medium;
+  font-size: 30px;
+  color: #222222;
+  line-height: 32px;
+  font-weight: bold;
+  margin-left: 0.5px;
+}
+
+.gray-size24-t9 {
+  position: relative;
+  align-self: center;
+  font-family: PingFangSC-Regular;
+  font-size: 24px;
+  color: #888888;
+  line-height: 28px;
+  font-weight: normal;
+  margin-top: 8px;
+  margin-right: 0.5px;
+}
+
+.w126-h122-i7 {
+  position: relative;
+  width: 126px;
+  height: 122px;
+  align-self: center;
+  resize: stretch;
+  margin-top: 16px;
+  margin-left: 0.5px;
+}
+
+.white-column-center-g40 {
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  align-self: flex-start;
+  background-color: rgba(245, 246, 247, 1);
+  border-radius: 12px;
+  margin-left: 16px;
+  padding-top: 22px;
+  padding-right: 13.5px;
+  padding-bottom: 20px;
+  padding-left: 13px;
+  display: flex;
+}
+
+.black-size30-t4 {
+  position: relative;
+  align-self: center;
+  font-family: PingFangSC-Medium;
+  font-size: 30px;
+  color: #222222;
+  line-height: 32px;
+  font-weight: bold;
+}
+
+.gray-size24-t5 {
+  position: relative;
+  align-self: center;
+  font-family: PingFangSC-Regular;
+  font-size: 24px;
+  color: #888888;
+  line-height: 28px;
+  font-weight: normal;
+  margin-top: 8px;
+  margin-right: 0.5px;
+}
+
+.w124-h126-i3 {
+  position: relative;
+  width: 124px;
+  height: 126px;
+  align-self: center;
+  resize: stretch;
+  margin-top: 12px;
+  margin-right: 1px;
+}
+
+.w36-h12-i1 {
+  position: relative;
+  width: 36px;
+  height: 12px;
+  align-self: center;
+  resize: stretch;
+  margin-top: 18px;
+  margin-bottom: 18px;
+}
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {};
+  },
+  components: {},
+  methods: {},
+  created: function() {}
+};
+</script>
